17a4ed3f351b2afe089e0357c86b1e3a2b0cda4d
Taufiq <htaufiq225@gmail.com>
2024-05-17 18:59:01 +0800

    setup database

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/test-db-connection', function() {
-//     try{
-//         DB::connection()->getPdo();
-//         return 'Koneksi database berhasil';
-//     } catch(\Exception $e) {
-//         return "tidak dapat terhubung ke database:" . $e->getMessage();
-//     }
-// });
+Route::get('/test-db-connection', function() {
+    try{
+        DB::connection()->getPdo();
+        return 'Koneksi database berhasil';
+    } catch(\Exception $e) {
+        return "tidak dapat terhubung ke database:" . $e->getMessage();
+    }
+});
