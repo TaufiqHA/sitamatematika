9f7a766826728a05689ab62955b865ff4125c3ff
Taufiq <htaufiq225@gmail.com>
2024-05-17 18:45:59 +0800

    setup database

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/test-db-connection', function() {
+//     try{
+//         DB::connection()->getPdo();
+//         return 'Koneksi database berhasil';
+//     } catch(\Exception $e) {
+//         return "tidak dapat terhubung ke database:" . $e->getMessage();
+//     }
+// });
